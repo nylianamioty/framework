d92b9ed371f84d009719a0edbd63d44936c0eff4
nylianamioty <miotyrazafimahefa@gmail.com>
2025-11-10 23:23:08 +0300

    sprint3

---
+++ src/main/java/com/monframework/annotation/Controller.java
@@ -0,0 +1,13 @@
+package com.monframework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Controller
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Controller {
+    String value() default "";
+}
